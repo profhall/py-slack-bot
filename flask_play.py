5b037b50513741a64be5c21a42dbef7db1b348af
Professor Hall <iprofhall@gmail.com>
2018-08-11 12:40:57 -0400

    events page is working now, had to change GET to POST

--- flask_play.py
+++ flask_play.py
@@ -1,11 +1,11 @@
 # import dependencies
 import os, sys, json, time, requests
 from flask import Flask, request, Response, jsonify, make_response
-from THD_MD.marketStores import market_stores
-from THD_MD.store_info import store_info
-from THD_MD.getMarkets import listMarkets
+#from THD_MD.marketStores import market_stores
+#from THD_MD.store_info import store_info
+#from THD_MD.getMarkets import listMarkets
 from slack_botClass import slackCommunication
-from THD_MD.generateToken import head
+#from THD_MD.generateToken import head
 
 
 # bootstrap the app
@@ -30,38 +30,17 @@ def anotherPage():
 
 @app.route('/test2')
 def yetAnotherPage():
-    store = store_info(store_number=114)
+    #store = store_info(store_number=114)
     return 'On Yet Another Page %s' % store
 
-@app.route('/events', methods=['Get'])
+@app.route('/events', methods=['POST', 'GET'])
 def events():
-    challenge = request.args.get('challenge')
-    token = request.args.get('token')
-    type = request.args.get('type')
-    data = {'challenge': challenge}
-    js = json.dumps(data)
+    slack_message = request.data
+    if(slack_message.get('type') == 'url_verification'):
+        print('url verification')
 
-    my_resp = make_response('response')
-    my_resp.headers['challenge'] = challenge
-    my_resp.status_code = 200
-
-    resp = Response(js, status=200, mimetype='application/json')
-    resp.headers['token'] = token
-    resp.headers['type'] = type
-    resp.headers['challenge'] = challenge
-    return (resp)
+    return (request.data)
 #https://myapp-optimistic-topi.apps-np.homedepot.com/events/
-
-@app.route('/hello')
-def hello():
-    markets_url = 'https://master-data-location.apps-np.homedepot.com/location/markets'
-
-    response = requests.get(markets_url, headers=head)
-    markets_json = response.json()
-
-    total_pages = markets_json['page']['totalPages']
-    # print (json.dumps(markets_json, indent=1))
-    return json.dumps(markets_json, indent=1)
 
 
 # start the app
